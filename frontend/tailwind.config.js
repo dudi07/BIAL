6abf02ae035b8aeaadaf80a51f328697eea676d9
Sanjay Dudi <61935787+dudi07@users.noreply.github.com>
2022-01-09 21:57:26 +0530

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    backgroundImage: {
+      cabBG: "url('/public/cabBG.jpg')",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
